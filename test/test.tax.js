f15324a0d677e8929b36e6d53837eb7ee6c0ada7
Alfie Darko <me@alfiedarko.co.uk>
2018-06-04 20:19:21 +0100

    GREEN: Tax > .applyTax() > returns correct applied taxrate of 8.64% on miltiple totals

--- test/test.tax.js
+++ test/test.tax.js
@@ -2,12 +2,19 @@
 
 describe('Tax', function() {
   describe('.applyTax()', function() {
+
     it('returns applied taxrate of 8.64% on the total', function() {
       let tax = new Tax()
 
       let calculate = sinon.stub(Total.prototype, "calculate").returns(4.75)
-      console.log(calculate());
       expect(tax.applyTax(calculate())).to.eql(5.16)
+    });
+
+    it('returns correct applied taxrate of 8.64% on multiple totals', function() {
+      let tax = new Tax()
+      Total.prototype.calculate.restore(); // Unwraps the spy
+      let calculate = sinon.stub(Total.prototype, "calculate").returns(20.40)
+      expect(tax.applyTax(calculate())).to.eql(22.16)
     });
   });
 });
