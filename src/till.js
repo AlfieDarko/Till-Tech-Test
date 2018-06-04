d5c82db0c4fafe06467c655b580d2d8e97a9f5dc
Alfie Darko <mrhazzy@gmail.com>
2018-06-04 02:07:26 +0100

    GREEN: Till > .calculateTotal > returns total from of 1 item of the basket

--- src/till.js
+++ src/till.js
@@ -1,9 +1,25 @@
 (function(exports) {
 	function Till() {
+		this.orders = new Orders();
 		this.total = 0;
 	}
 
 	Till.prototype.calculateTotal = function() {
+		let items = this.orders.showBasket()[0].items;
+		// loop throughh items
+		// if key exist in product obj~
+		// add return value to this.tootal
+		// return this.total
+
+		for (let i of items) {
+			console.log(items);
+			if (this.orders.products.hasOwnProperty(i)) {
+				console.log(this.orders.products[i]);
+				console.log(this.total);
+				this.total += this.orders.products[i];
+			}
+		}
+
 		return this.total;
 	};
 	exports.Till = Till;
--- test/test.till.js
+++ test/test.till.js
@@ -1,7 +1,6 @@
 /* eslint-env es6, mocha */
-
 describe("Till", function() {
-	describe(".total", () => {
+	describe(".total", function() {
 		it("is a property that holds the total", () => {
 			let till = new Till();
 			expect(till.total).to.eql(0);
@@ -9,10 +8,22 @@ describe("Till", function() {
 	});
 
 	describe(".calculateTotal()", function() {
-		it("returns total Orders.basket()", () => {
+		it("returns total of 1 item from the basket", function() {
+			// create a fake Orders class
+			// create a fake basket method
+			// call calculateTotal on our fake basket to return total
+
 			let till = new Till();
 
-			expect(till.calculateTotal()).to.eql(3.85);
+			let basket = sinon.stub(Orders.prototype, "showBasket").returns([
+				{
+					name: "Tom",
+					items: ["Cafe Latte"]
+				}
+			]);
+			// let tomsOrder = ;
+			// stub.returns(tomsOrder);
+			expect(till.calculateTotal()).to.eql(4.75);
 		});
 	});
 });
