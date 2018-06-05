d0ae4afe305f928ade8640adc53120780a6ba185
Alfie Darko <me@alfiedarko.co.uk>
2018-06-05 12:54:16 +0100

    REFACTOR: DRY refactor of test.receipt.js

--- test/test.receipt.js
+++ test/test.receipt.js
@@ -4,51 +4,41 @@ describe('Receipt', function() {
   let receipt
   let showBasket
 
-  describe('.printReceipt', function() {
+  describe('.printReceipt()', function() {
     beforeEach(function() {
       receipt = new Receipt()
       showBasket = sinon.stub(Orders.prototype, "showBasket")
+
+      showBasket.returns({
+        name: "Tom",
+        items: ["Cafe Latte", "Single Espresso"]
+      })
+
     });
 
     afterEach(function() {
       Orders.prototype.showBasket.restore()
     });
-    it('prints a receipt including the correct name of order', function() {
-      showBasket.returns({
-        name: "Tom",
-        items: ["Cafe Latte", "Single Espresso"]
-      })
 
+    it('prints a receipt including the correct name of order', function() {
       expect(receipt.printReceipt(showBasket())).to.include('Tom')
     });
 
     it('prints a receipt including the correct price of order', function() {
-      showBasket.returns({
-        name: "Tom",
-        items: ["Cafe Latte", "Single Espresso"]
-      })
-
       expect(receipt.printReceipt(showBasket())).to.include(6.80)
     });
 
     it('prints a receipt including the correct items ', function() {
-      showBasket.returns({
-        name: "Tom",
-        items: ["Cafe Latte", "Single Espresso"]
-      })
-
       expect(receipt.printReceipt(showBasket())).to.include('Cafe Latte')
     });
 
-    it('prints a receipt not including correct items ', function() {
-      showBasket.returns({
-        name: "Tom",
-        items: ["Cafe Latte", "Single Espresso"]
-      })
-
+    it('prints a receipt not including incorrect items ', function() {
       expect(receipt.printReceipt(showBasket())).to.not.include('Banana')
     });
 
+    // it('includes cost of each item serperately', function() {
+    //   expect(receipt.printReceipt(showBasket())).to.include('Cafe Late - 4.75')
+    // });
   });
 
 });
