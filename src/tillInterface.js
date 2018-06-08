ac9a7ee49e10c37b1226e5e4fe46bd16f499a9fe
Alfie Darko <me@alfiedarko.co.uk>
2018-06-08 13:21:36 +0100

    VIEWS: Add to cart now updates the cart dropdown options with basket items

--- src/tillInterface.js
+++ src/tillInterface.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
   let till = new Till()
 
   let customerNameFromInput
+  let cartItems = $('#cart-with-items')
+  console.log(cartItems);
 
-  $( "#add-name-button" ).click(function() {
+  $("#add-name-button").click(function() {
     if ($("#customer-names-input").val()) {
       customerNameFromInput = $("#customer-names-input").val()
       $("#customer-names-input").prop("disabled", true);
       $("#add-name-button").prop("disabled", true);
-
     }
     console.log(customerNameFromInput);
   });
@@ -23,22 +24,19 @@ $(document).ready(function(){
       $("#items-in-dropdown option:selected").each(function(i, sel) {
         itemsArray.push($(sel).val())
       });
+      // console.log(itemsArray);
+      till.orders.basket.push({name: customerNameFromInput, items: itemsArray})
+      // console.log(till.orders.showBasket());
 
-      console.log(itemsArray);
-
-      till.orders.basket.push({
-        name: customerNameFromInput,
-        items: itemsArray
+      till.orders.showBasket()[0].items.forEach(function(item) {
+        let option = document.createElement("option")
+        option.textContent = item;
+        document.getElementById('cart-with-items').appendChild(option)
       })
 
-      console.log(till.orders.showBasket());
-
+      // console.log(cartItems);
 
     }
 
-
-
-  })
-
-
-});
+  });
+})
