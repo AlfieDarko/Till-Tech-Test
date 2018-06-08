35c76bdb1415cdf47a533d9b9c5e9a87eff716fc
Alfie Darko <me@alfiedarko.co.uk>
2018-06-08 14:24:14 +0100

    VIEWS: Clear cart button clears the basket array and removes the items from the cart page

--- src/tillInterface.js
+++ src/tillInterface.js
@@ -18,25 +18,31 @@ $(document).ready(function() {
     if (customerNameFromInput == null) {
       alert('Enter the customer name!')
     } else {
-
       let itemsArray = []
-
       $("#items-in-dropdown option:selected").each(function(i, sel) {
         itemsArray.push($(sel).val())
       });
       // console.log(itemsArray);
       till.orders.basket.push({name: customerNameFromInput, items: itemsArray})
       // console.log(till.orders.showBasket());
-
       till.orders.showBasket()[0].items.forEach(function(item) {
         let option = document.createElement("option")
+        console.log(option);
         option.textContent = item;
+        option.disabled = true
         document.getElementById('cart-with-items').appendChild(option)
       })
 
-      // console.log(cartItems);
-
+      $("#add-to-cart-button").prop("disabled", true)
     }
-
   });
+
+  $("#clear-cart-button").click(function() {
+
+    till.orders.clearBasket()
+
+    $('#cart-with-items').find('option').remove().end()
+    $("#add-to-cart-button").prop("disabled", false)
+
+  })
 })
