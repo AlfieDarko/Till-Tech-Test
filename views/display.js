37adef938d82022e9b7f3f4195528809b41aef99
Alfie Darko <me@alfiedarko.co.uk>
2018-06-18 13:59:43 +0100

    GREEN & REFACTOR: Fixed receipt class bug

--- views/display.js
+++ views/display.js
@@ -1,7 +1,27 @@
 let display = {
   viewTotal: function() {
-    let totalCalc = till.receipt.total.calculate(till.basket.returnBasket());
+    let basket = till.basket.returnBasket();
+    let totalCalculated = till.receipt.total.calculate(basket);
+    console.log(totalCalculated);
+    let appliedDisc = till.receipt.discount.applyDiscounts(
+      totalCalculated,
+      basket
+    );
+    console.log(appliedDisc);
+    let itemsTotalDiscounts = till.receipt.discount.applyDiscounts(
+      totalCalculated,
+      basket
+    );
+    console.log(itemsTotalDiscounts);
+    let itemsTotalWithoutTax = till.receipt.total.calculate(basket).toFixed(2);
+    console.log(itemsTotalWithoutTax);
+    let appliedTax = till.receipt.tax.applyTax(totalCalculated).toFixed(2);
+    console.log(appliedTax);
+    let amountToTax = appliedTax - totalCalculated;
 
-    $("#total-text-area").val(totalCalc.toFixed(2));
+    let totalWithTax =
+      parseFloat(itemsTotalWithoutTax) + parseFloat(amountToTax);
+    console.log(totalWithTax);
+    $("#total-text-area").val(totalWithTax.toFixed(2));
   }
 };
