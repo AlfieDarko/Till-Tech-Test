ef08b40f721359a2ed9941e9b4c84b50483ae3e5
Alfie Darko <me@alfiedarko.co.uk>
2018-06-18 10:06:21 +0100

    REFACTOR: Basket > addtoBasket(). Changed internal datastructure to just accept object and push into basket property

--- src/basket.js
+++ src/basket.js
@@ -1,39 +1,38 @@
 (function(exports) {
   function Basket() {}
 
-  const _basket = []
+  const _basket = [];
 
   Basket.prototype.clearBasket = function() {
-    _basket.length = 0
+    _basket.length = 0;
   };
 
   Basket.prototype.returnBasket = function() {
     if (_basket[0] === undefined) {
-      return _basket
+      return _basket;
     } else {
       return _basket[0];
     }
   };
 
-  Basket.prototype.addToBasket = function(name, food) {
+  Basket.prototype.addToBasket = function(order) {
     // takes a name, and take a food item
     // TO FIX BUG, JUST ACCEPT OBJECT AS IT IS AND PUSH INTO BASKET!!
 
     // so we can extend the scope of this inside of internal functions
-    let self = this;
+    // let self = this;
 
     // so we can skip the name in the arguments and push the food into an array
-    let args = Array.from(arguments)
-    .slice(1)
-
+    // let args = Array.from(arguments)
+    // .slice(1)
 
     // Now ww will create an newOrder object with our items and name
-    let newOrder = {
-      name,
-      items: [...args]
-    };
+    // let newOrder = {
+    //   name,
+    //   items: [...args]
+    // };
 
-    _basket.push(newOrder);
+    _basket.push(order);
   };
 
   exports.Basket = Basket;
--- test/test.basket.js
+++ test/test.basket.js
@@ -13,33 +13,27 @@ describe("Basket", function() {
   describe(".addToBasket()", () => {
     beforeEach(function() {
       basket = new Basket();
-      basket.clearBasket()
+      basket.clearBasket();
     });
 
     it("adds a name and food item to the order object in the basket", () => {
-      let tomsOrder =
-        {
-          name: "Tom",
-          items: ["Cafe Latte"]
-        }
-      ;
-      basket.addToBasket("Tom", "Cafe Latte");
+      let tomsOrder = {
+        name: "Tom",
+        items: ["Cafe Latte"]
+      };
+      basket.addToBasket(tomsOrder);
       expect(basket.returnBasket()).to.eql(tomsOrder);
     });
 
     it("adds a name and multiple food items to the order object", () => {
-      let annasOrder =
-        {
-          name: "Anna",
-          items: ["Cafe Latte", "Single Espresso", "Cortado"]
-        }
-      ;
-      basket.addToBasket("Anna", "Cafe Latte", "Single Espresso", "Cortado");
+      let annasOrder = {
+        name: "Anna",
+        items: ["Cafe Latte", "Single Espresso", "Cortado"]
+      };
+      basket.addToBasket(annasOrder);
       expect(basket.returnBasket()).to.eql(annasOrder);
     });
   });
-
-
 
   describe(".clearBasket()", function() {
     beforeEach(function() {
