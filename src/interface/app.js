4e5ef9510b23b6dbdd77ec0d51b7aee2f69675bb
Alfie Darko <me@alfiedarko.co.uk>
2018-06-21 14:08:01 +0100

    removed click events and moved them to interface.js file

--- src/interface/app.js
+++ src/interface/app.js
@@ -9,32 +9,3 @@ let till = new Till(basket, payments, receipt);
 let display = new Display();
 let buttons = new Buttons();
 // is this the proper way to declare instances in an app like this?
-$(document).ready(function() {
-  $("#add-name-button").click(function() {
-    buttons.addName();
-  });
-
-  $("#add-to-cart-button").click(function() {
-    buttons.addToCart();
-  });
-
-  $("#clear-cart-button").click(function() {
-    buttons.clearCart();
-  });
-
-  $("#pay-button").click(function() {
-    $(".modal").addClass("is-active");
-  });
-
-  $("#submit-to-till-button").click(function() {
-    buttons.submitToTill();
-  });
-
-  $("#cancel-submit-button").click(function() {
-    buttons.cancelPayment();
-  });
-
-  $(".delete").click(function() {
-    $(".modal").removeClass("is-active");
-  });
-});
