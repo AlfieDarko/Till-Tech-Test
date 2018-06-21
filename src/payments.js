56597f80b42498fc41738c1c757d9192c98d7882
Alfie Darko <me@alfiedarko.co.uk>
2018-06-21 14:08:36 +0100

    changed else if statements to multiple if statements for clarity

--- src/payments.js
+++ src/payments.js
@@ -1,7 +1,6 @@
 (function(exports) {
   class Payments {
-    constructor() {
-    }
+    constructor() {}
     setExpectedPayment(postTaxAmount) {
       _expectedPayment = postTaxAmount;
       return _expectedPayment;
@@ -10,13 +9,11 @@
       if (isNaN(amount)) {
         throw new Error("Not a valid number");
       }
-      else if (amount < _expectedPayment) {
+      if (amount < _expectedPayment) {
         throw new Error("Not enough to make payment");
       }
-      else {
-        _changeExpected = amount - _expectedPayment;
-        return true;
-      }
+      _changeExpected = amount - _expectedPayment;
+      return true;
     }
     returnChange() {
       return parseFloat(_changeExpected.toFixed(2));
@@ -25,9 +22,6 @@
 
   let _expectedPayment = 0;
   let _changeExpected = 0;
-
-
-
 
   exports.Payments = Payments;
 })(this);
