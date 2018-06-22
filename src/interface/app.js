dc16c1fa047a13d2b78e4f2305cc90797716491e
Alfie Darko <me@alfiedarko.co.uk>
2018-06-22 16:47:50 +0100

    removed clickevents from app..js

--- src/interface/app.js
+++ src/interface/app.js
@@ -6,6 +6,5 @@ let tax = new Tax();
 let discount = new Discount();
 let receipt = new Receipt(total, tax, discount);
 let till = new Till(basket, payments, receipt);
-let display = new Display();
+let display = new Display()
 let buttons = new Buttons();
-// is this the proper way to declare instances in an app like this?
