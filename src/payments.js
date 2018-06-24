e1bee5e312eb3c356932fbb02b5f9ed1204fe1c3
Alfie Darko <mrhazzy@gmail.com>
2018-06-24 19:11:55 +0100

    REFACTOR: Changed parameter name amount to amountGiven for more clarification

--- src/payments.js
+++ src/payments.js
@@ -5,15 +5,15 @@
       _expectedPayment = postTaxAmount;
       return _expectedPayment;
     }
-    takePayment(amount) {
-      if (isNaN(amount)) {
+    takePayment(amountGiven) {
+      if (isNaN(amountGiven)) {
         throw new Error("Not a valid number");
       }
-      if (amount < _expectedPayment) {
+      if (amountGiven < _expectedPayment) {
         throw new Error("Not enough to make payment");
       }
-      _amountGiven = amount
-      _changeExpected = amount - _expectedPayment;
+      _amountGiven = amountGiven
+      _changeExpected = amountGiven - _expectedPayment;
       return true;
     }
     returnChange() {
