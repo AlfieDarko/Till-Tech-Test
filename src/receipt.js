b75c3504acdde3e40aac856275f2894c7b4bc273
Alfie Darko <me@alfiedarko.co.uk>
2018-06-25 14:26:27 +0100

    Changed reference form globals to injected dependancy

--- src/receipt.js
+++ src/receipt.js
@@ -33,14 +33,14 @@
 
     addDiscountAlertOnReceipt(args, totalCalculated) {
       if (
-        discount.returnMuffinDiscountBool(args) &&
-        discount.returnSpendOver50DiscountBool(totalCalculated)
+        this.discount.returnMuffinDiscountBool(args) &&
+        this.discount.returnSpendOver50DiscountBool(totalCalculated)
       ) {
         _receiptArray.push(
           "10% Muffin Discount!",
           "5% Off Purchases over 50 Discount!"
         );
-      } else if (discount.returnMuffinDiscountBool(args)) {
+      } else if (this.discount.returnMuffinDiscountBool(args)) {
         _receiptArray.push("10% Muffin Discount!");
       } else if (discount.returnSpendOver50DiscountBool(totalCalculated)) {
         _receiptArray.push("5% Off Purchases over 50 Discount!");
@@ -51,22 +51,24 @@
       this.addShopDetailsToReceipt()
       this.addDateTimeToReceipt()
       // set up variable names for clarification
-      let preTotal = total.calculate(basket).toFixed(2);
-      let preTotalWithDiscounts = discount.applyDiscounts(preTotal, basket);
+      let preTotal = this.total.calculate(basket).toFixed(2);
+      let preTotalWithDiscounts = this.discount.applyDiscounts(preTotal, basket);
       _receiptArray.push(`${basket.name}'s Order:`);
       let amountToTax = (
-        tax.applyTax(total.calculate(basket)).toFixed(2) - preTotal
+        this.tax.applyTax(total.calculate(basket)).toFixed(2) - preTotal
       ).toFixed(2);
       let totalWithTax =
         parseFloat(preTotalWithDiscounts) + parseFloat(amountToTax);
       // This zips-up item and price together as a line item and push into receipt array
-      this.zipItemsAndPrices(basket.items, total.calculateLinePrice(basket)).map(
+      this.zipItemsAndPrices(basket.items, this.total.calculateLinePrice(basket)).map(
         lineItem => {
-          _receiptArray.push(`${lineItem[0]}: £${lineItem[1]}`);
+          _receiptArray.push(`${lineItem[0]}: £${lineItem[1]}`)
         }
       );
+
       this.addDiscountAlertOnReceipt(basket, preTotal);
-      discount.resetDiscounts();
+      this.discount.resetDiscounts();
+
       _receiptArray.push(
         `==================`,
         `  Total: £${preTotal}`,
